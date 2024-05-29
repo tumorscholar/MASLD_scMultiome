5d6a8bc6068451d33f8220fe2e29bbb89c350c01
Raju Kumar <156453454+tumorscholar@users.noreply.github.com>
2024-05-29 16:53:06 +0100

    Update loadData.R

--- loadData.R
+++ loadData.R
@@ -1,27 +1,69 @@
-# This script is for the loading and prepreocessing of scRNAseq datasets using Seurat
+# This script is for the loading and prepreocessing of CITEseq datasets using Seurat
 
 # Set working directory
 setwd('...')
 
 # Load packages
 library(Seurat)
+library(hdf5r)
 
 # Locate files 
-dataDir <- '...'
+dataDir <- '/data/Blizard-AlazawiLab/rk/cellranger_counts_rk'
 files <- list.files(path = dataDir,
-                    pattern = '*.h5',
+                    pattern = 'filtered_feature_bc_matrix.h5',
                     recursive = T,
                     full.names = T)
+files <- files[-c(10,16,18,19,28,30,32)]
 
 # Extract sample names from file names
-pattern <- '...'
-samples <- gsub(pattern,
+pattern1 <- '/data/Blizard-AlazawiLab/rk/cellranger_counts_rk/'
+samples <- gsub(pattern1,
                 '',
                 files)
 
+pattern2 <- '/outs/filtered_feature_bc_matrix.h5'
+samples <- gsub(pattern2,
+                '',
+                samples)
+
+pattern3 <- '/count/sample_filtered_feature_bc_matrix.h5'
+samples <- gsub(pattern3,
+                '',
+                samples)
+
+pattern4 <- 'new_samples/Analysis/Seurat_Demux/h5/'
+samples <- gsub(pattern4,
+                '',
+                samples)
+
+pattern5 <- '_sample_filtered_feature_bc_matrix.h5'
+samples <- gsub(pattern5,
+                '',
+                samples)
+
+pattern6 <- 'new_samples/Analysis/CellRanger_Total/'
+samples <- gsub(pattern6,
+                '',
+                samples)
+
+pattern7 <- 'GC-WL-10634-LIVER/outs/per_sample_outs/'
+samples <- gsub(pattern7,
+                '',
+                samples)
+
+pattern8 <- 'GC-WL-10738-LIVER/outs/per_sample_outs/'
+samples <- gsub(pattern8,
+                '',
+                samples)
+
+pattern9 <- 'GC-WL-10742-LIVER/outs/per_sample_outs/'
+samples <- gsub(pattern9,
+                '',
+                samples)
+
 # Load all data
 rawDat <- lapply(files, function(x){
-  Read10X_h5(x)
+ Read10X_h5(x)
 }) 
 
 # Specify list element names 
@@ -29,15 +71,17 @@ names(rawDat) <- samples
 
 # Make seurat objects in for loop
 datObjs <- list() 
-for (x in samples) {
-  
-  # Create seurat object
-  datObjs[[x]] <- CreateSeuratObject(counts = rawDat[[x]][['Gene Expression']],
-                                     project = x)
-  
-  # Add antibody to new assay object
-  datObjs[[x]][['ADT']] <- CreateAssayObject(counts = rawDat[[x]][['Antibody Capture']])
-  
-  datObjs[[x]]@meta.data$sample <- x
-  
+for (x in samples[-c(10,16,18,19,28,30,32)]) {
+ 
+ # Create seurat object
+ datObjs[[x]] <- CreateSeuratObject(counts = rawDat[[x]][['Gene Expression']],
+                                    project = x)
+ 
+ # Add antibody to new assay object
+ datObjs[[x]][['ADT']] <- CreateAssayObject(counts = rawDat[[x]][['Antibody Capture']])
+ 
+ datObjs[[x]]@meta.data$sample <- x
+ 
 }
+
+
