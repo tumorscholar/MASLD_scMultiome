716ebe27eb112cbc3e86c3398e09e8a9bb3000d2
Raju Kumar <156453454+tumorscholar@users.noreply.github.com>
2024-06-03 10:40:16 +0100

    Update loadData.R

--- loadData.R
+++ loadData.R
@@ -8,56 +8,20 @@ library(Seurat)
 library(hdf5r)
 
 # Locate files 
-dataDir <- '/data/Blizard-AlazawiLab/rk/cellranger_counts_rk'
+dataDir <- '/data/Blizard-AlazawiLab/rk/cellranger'
 files <- list.files(path = dataDir,
                     pattern = 'filtered_feature_bc_matrix.h5',
                     recursive = T,
                     full.names = T)
-files <- files[-c(10,16,18,19,28,30,32)]
 
 # Extract sample names from file names
-pattern1 <- '/data/Blizard-AlazawiLab/rk/cellranger_counts_rk/'
+pattern1 <- '/data/Blizard-AlazawiLab/rk/cellranger/'
 samples <- gsub(pattern1,
                 '',
                 files)
 
 pattern2 <- '/outs/filtered_feature_bc_matrix.h5'
 samples <- gsub(pattern2,
-                '',
-                samples)
-
-pattern3 <- '/count/sample_filtered_feature_bc_matrix.h5'
-samples <- gsub(pattern3,
-                '',
-                samples)
-
-pattern4 <- 'new_samples/Analysis/Seurat_Demux/h5/'
-samples <- gsub(pattern4,
-                '',
-                samples)
-
-pattern5 <- '_sample_filtered_feature_bc_matrix.h5'
-samples <- gsub(pattern5,
-                '',
-                samples)
-
-pattern6 <- 'new_samples/Analysis/CellRanger_Total/'
-samples <- gsub(pattern6,
-                '',
-                samples)
-
-pattern7 <- 'GC-WL-10634-LIVER/outs/per_sample_outs/'
-samples <- gsub(pattern7,
-                '',
-                samples)
-
-pattern8 <- 'GC-WL-10738-LIVER/outs/per_sample_outs/'
-samples <- gsub(pattern8,
-                '',
-                samples)
-
-pattern9 <- 'GC-WL-10742-LIVER/outs/per_sample_outs/'
-samples <- gsub(pattern9,
                 '',
                 samples)
 
@@ -71,7 +35,7 @@ names(rawDat) <- samples
 
 # Make seurat objects in for loop
 datObjs <- list() 
-for (x in samples[-c(10,16,18,19,28,30,32)]) {
+for (x in samples) {
  
  # Create seurat object
  datObjs[[x]] <- CreateSeuratObject(counts = rawDat[[x]][['Gene Expression']],
@@ -83,5 +47,4 @@ for (x in samples[-c(10,16,18,19,28,30,32)]) {
  datObjs[[x]]@meta.data$sample <- x
  
 }
-
 
