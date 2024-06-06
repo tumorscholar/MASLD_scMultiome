b16bffe955c922b773b29334f527dd320d3d4e17
Raju Kumar <156453454+tumorscholar@users.noreply.github.com>
2024-06-06 13:12:29 +0100

    loadDataQC.R

--- loadData.R
+++ loadData.R
@@ -1,11 +1,12 @@
 # This script is for the loading and prepreocessing of CITEseq datasets using Seurat
 
 # Set working directory
-setwd('...')
+setwd("~/Seurat")
 
 # Load packages
 library(Seurat)
 library(hdf5r)
+library(ggplot2)
 
 # Locate files 
 dataDir <- '/data/Blizard-AlazawiLab/rk/cellranger'
@@ -38,13 +39,47 @@ datObjs <- list()
 for (x in samples) {
  
  # Create seurat object
- datObjs[[x]] <- CreateSeuratObject(counts = rawDat[[x]][['Gene Expression']],
-                                    project = x)
+ datObjs[[x]] <- CreateSeuratObject(counts = rawDat[[x]][['Gene Expression']], project = x)
  
  # Add antibody to new assay object
  datObjs[[x]][['ADT']] <- CreateAssayObject(counts = rawDat[[x]][['Antibody Capture']])
  
  datObjs[[x]]@meta.data$sample <- x
  
+ # Plot mitochondria and hemoglobin genes to remove dead and doublet cells
+ datObjs[[x]][["percent.mt"]] <- PercentageFeatureSet(datObjs[[x]], pattern = "^MT-")
+ features <- c('HBA1','HBA2','HBB')
+ datObjs[[x]][["percent.hb"]] <- PercentageFeatureSet(datObjs[[x]], features = features)
+ plt <- VlnPlot(datObjs[[x]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.hb"), ncol = 4)
+ ggsave(
+  plot = plt,
+  filename = paste0(x, '_qcPlot.tiff'),
+  height = 8,
+  width = 8,
+  dpi = 300,
+  units = 'in'
+ )
+ plt <- FeatureScatter(datObjs[[x]], feature1 = "nCount_RNA", feature2 = "nFeature_RNA") + geom_smooth(method = 'lm')
+ ggsave(
+  plot = plt,
+  filename = paste0(x, '_lmPlot.tiff'),
+  height = 8,
+  width = 8,
+  dpi = 300,
+  units = 'in'
+ )
+ # Filtering
+ datObjs[[x]] <- subset(datObjs[[x]], subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.hb < 10 & percent.mt < 10)
+ plt <-VlnPlot(datObjs[[x]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.hb"), ncol = 4)
+ ggsave(
+  plot = plt,
+  filename = paste0(x, '_afterQCplot.tiff'),
+  height = 8,
+  width = 8,
+  dpi = 300,
+  units = 'in'
+ )
 }
+
+saveRDS(datObjs, 'SeuObj.rds')
 
